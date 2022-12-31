dcf3b3f4c447b3362ca8a7837b848973d59658c8
Marcellin Wabo <marcellin@wabo.work>
2022-12-31 14:21:43 +0100

    Remove medoo ORM

--- libs/Logis_Log.php
+++ libs/Logis_Log.php
@@ -17,7 +17,7 @@ class Pesa_Log
     public function _log($to_write_to_logs)
     {
         $this->logger = new Logger('pesa');
-        $this->logger->pushHandler(new StreamHandler(_LOG_DIR_ . 'pesa.logs', Logger::DEBUG));
+        $this->logger->pushHandler(new StreamHandler(_LOG_DIR_ . 'logis.logs', Logger::DEBUG));
         $this->logger->info($to_write_to_logs);
     }
 }
